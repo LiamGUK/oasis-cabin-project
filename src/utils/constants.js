fdf1aeb2324b522cb63b6582113e7f90c991339e
Liam Groves <93141441+LiamGUK@users.noreply.github.com>
2024-01-17 22:12:02 +0000

    Add files via upload

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+// JS sheet for constant variables to use and share throughout app
+
+// Variable constant for how many results to display in each page of pagination set.
+export const PAGE_SIZE = 10;
